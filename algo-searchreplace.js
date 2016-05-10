9e5363cc1945d8e46425e668eadd9fb5f6df8f73
lanceseo <lance9980@gmail.com>
2016-05-09 20:53:46 -0700

    check 1st letter for case, match it with replacement

--- algo-searchreplace.js
+++ algo-searchreplace.js
@@ -1,20 +1,26 @@
 
 $("document").ready(function() {
 
+
 function myReplace(str, before, after) {
   var strArr = str.split(" ");
-  console.log(strArr);
+  //console.log(strArr);
   for (var i=0; i<strArr.length; i++) {
     if (strArr[i] === before) {
-       strArr[i] = after; 
-    }  
-  }
-  console.log(strArr);
+      if (strArr[i].charAt(0) === strArr[i].charAt(0).toUpperCase()) {      
+        var nStr = after.charAt(0).toUpperCase().concat(after.slice(1,after.length));
+        strArr[i] = nStr;
+      } else {
+        strArr[i] = after;
+      }
+     } 
+   }
+  console.log(strArr.join(" "));
     
   //return str;
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
 
 
 });
