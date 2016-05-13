ebd8be86730caab2f066b2e73649e391143db221
lanceseo <lance9980@gmail.com>
2016-05-12 22:48:42 -0700

    solved

--- algo-missingletters.js
+++ algo-missingletters.js
@@ -1,5 +1,17 @@
 function fearNotLetter(str) {
-  return str;
+  var cFirst = str.charCodeAt(0);
+  var cLast = str.charCodeAt(str.length-1);
+  var cLength = cLast - cFirst;
+  
+  for (var i=1; i<cLength; i++) {
+    if (str.charCodeAt(i) !== cFirst+i) {
+      var mStr = String.fromCharCode(cFirst+i);
+      console.log(mStr);
+      return;
+    }
+  }
+  console.log('none missing');
+    
 }
 
-fearNotLetter("abce");
+fearNotLetter("abcdeg");
