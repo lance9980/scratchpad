9661fecb8843dce0ec7f9e0ffa57eb1defb493a8
lanceseo <lance9980@gmail.com>
2016-05-13 18:42:39 -0700

    solved

--- algo-boowho.js
+++ algo-boowho.js
@@ -1,7 +1,11 @@
 
 function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
-  return bool;
+  if (bool === true || bool === false) {
+    console.log('Yay bool');  
+  } else {
+    console.log('Nay bool');
+  }
 }
 
 booWho(null);
